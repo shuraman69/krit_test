000f6015219b54561674fc7bdcf4028bea96e520
Pavlov Ivan <pavlov@e-krit.ru>
2019-09-24 18:55:45 +0000

    Init

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="user-scalable=no">
+  <title>Scratch.Radio</title>
+  <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+<body>
+  <h1>Scratch<span class="red">.Radio</span></h1>
+  <!-- Stations -->
+  <div class="padding"></div>
+  <div id="station0" class="station">
+    <div class="title">
+      <div id="title0" class="subtitle"></div>
+      <div id="live0" class="live">LIVE</div>
+      <div id="song0" class="song"></div>
+      <div id="time0" class="time"></div>
+      <div id="playing0" class="playing">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+      </div>
+    </div>
+  </div>
+  <div id="station1" class="station">
+    <div class="title">
+      <div id="title1" class="subtitle"></div>
+      <div id="live1" class="live">LIVE</div>
+      <div id="song1" class="song"></div>
+      <div id="time1" class="time"></div>
+      <div id="playing1" class="playing">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+      </div>
+    </div>
+  </div>
+  <div id="station2" class="station">
+    <div class="title">
+      <div id="title2" class="subtitle"></div>
+      <div id="live2" class="live">LIVE</div>
+      <div id="song2" class="song"></div>
+      <div id="time2" class="time"></div>
+      <div id="playing2" class="playing">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+      </div>
+    </div>
+  </div>
+  <div class="padding"></div>
+
+  <!-- Scripts -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="./assets/js/howler/howler.core.min.js"></script>
+  <script src="./assets/js/radio.js"></script>
+</body>
+</html>
