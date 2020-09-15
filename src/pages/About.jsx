d982dc26ea9fc44ea3c6834e41625c137044b3ce
Pavlov Ivan <pavlov@e-krit.ru>
2020-09-15 17:23:17 +0000

    info meta update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,15 @@ function About() {
   return (
     <div className="container container--offset_top container--offset_left container--offset_bottom text-page">
       <div className="text-page__item">
-        <span className="title">Project</span>
+        <span className="title">About project</span>
         <p className="text text--offset_bottom">
-          This project is designed to make cool music more accessible.
+          This project is designed to make scratch music more accessible.
         </p>
-        <span className="text text--theme_gold text--size_sm">Try it.</span>
+        <span className="text text--theme_gold text--size_sm">Privacy statement</span>
+        <p className="text text--offset_bottom">
+          The authors of the project do not approve or call for anything, nor do they oppose or support it.
+          All broadcast music is the property of its authors and is provided to radio listeners free of charge.
+        </p>
       </div>
     </div>
   );
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -69,6 +69,8 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
+              //alert ('update me');
+              //registration.update();
               console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
