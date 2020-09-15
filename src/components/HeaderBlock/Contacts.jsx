8d8d1c40ecf88ea19427acce731b0c8a781d36b4
Pavlov Ivan <pavlov@e-krit.ru>
2020-09-15 15:49:19 +0000

    contacts

--- src/components/HeaderBlock/Contacts.jsx
+++ src/components/HeaderBlock/Contacts.jsx
@@ -4,11 +4,8 @@ function Contacts() {
   return (
     <div className="contacts">
       <span className="contacts__title">Contact us</span>
-      <a href="mailto:info@scratch.radio" className="link link--theme_gray contacts__link">
-        info@scratch.radio
-      </a>
-      <a href="/" className="link link--theme_gray contacts__link">
-        facebook
+      <a href="mailto:listen@scratch.radio" className="link link--theme_gray contacts__link">
+        listen@scratch.radio
       </a>
     </div>
   );
