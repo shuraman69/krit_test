43b910332a1ec41a46dc2f7c3a6d5c250435fba3
Pavlov Ivan <pavlov@e-krit.ru>
2020-12-01 17:44:57 +0000

    update on prod

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -5,7 +5,7 @@ function Update() {
     <div className="update text-center">
       <div className="update__inner">
         <span className="update__title">Congratulations, a new version is available!</span>
-        <p className="update__desc">We&apos;ve improved the app, please wait...</p>
+        <p className="update__desc">We are updating the app...</p>
         <div className="update-bar axis-x-center">
           <div className="update-bar__line" />
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -8,56 +8,35 @@ import reportWebVitals from './reportWebVitals';
 
 import './scss/app.scss';
 import App from './App';
-// import { Update } from './components';
+import { Update } from './components';
 import store from './redux/store';
 
-// const isUpdate = false;
-
 const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
-/* serviceWorker.register({
-  onUpdate: (registration) => {
-    isUpdate = true;
-    setTimeout(() => {
-      window.location.reload();
-    }, 5000);
-    if (registration && registration.waiting) {
-      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-    }
-  },
-}); */
 
 renderMethod(
   <BrowserRouter>
     <Provider store={store}>
-      {/* isUpdate && <Update /> */}
       <App />
     </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
 
-// Old
-// serviceWorker.register({
-//   onUpdate: (registration) => {
-//     isUpdate = true;
-//     setTimeout(() => {
-//       window.location.reload();
-//     }, 5000);
-//     if (registration && registration.waiting) {
-//       registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-//     }
-//   },
-// });
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    ReactDOM.render(<Update />, document.getElementById('root'));
+
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
