e33d5655b125028c1519f92169ddd9ba7ee63957
Pavlov Ivan <pavlov@e-krit.ru>
2020-12-01 16:12:51 +0000

    kill sw

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
-import * as serviceWorker from './serviceWorker';
-
+// import * as serviceWorker from './serviceWorker';
 import './scss/app.scss';
 import App from './App';
-import { Update } from './components';
+// import { Update } from './components';
 import store from './redux/store';
-
-let isUpdate = false;
-
+// let isUpdate = false;
 const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.register();
-serviceWorker.register({
+/* serviceWorker.register({
   onUpdate: (registration) => {
     isUpdate = true;
     setTimeout(() => {
@@ -28,12 +24,12 @@ serviceWorker.register({
       registration.waiting.postMessage({ type: 'SKIP_WAITING' });
     }
   },
-});
+}); */
 
 renderMethod(
   <BrowserRouter>
     <Provider store={store}>
-      {isUpdate && <Update />}
+      {/* isUpdate && <Update /> */}
       <App />
     </Provider>
   </BrowserRouter>,
