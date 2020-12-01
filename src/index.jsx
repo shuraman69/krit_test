5ae7d5fa7850d85a040a8f4b8691882dbbd7f86a
Pavel Konstantinov <pashakons@yandex.ru>
2020-12-01 19:39:39 +0300

    update app

--- src/index.jsx
+++ src/index.jsx
@@ -3,32 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 import './scss/app.scss';
 import App from './App';
 import { Update } from './components';
 import store from './redux/store';
 
-let isUpdate = false;
+const isUpdate = false;
 
 const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
-serviceWorker.register({
-  onUpdate: (registration) => {
-    isUpdate = true;
-    setTimeout(() => {
-      window.location.reload();
-    }, 5000);
-    if (registration && registration.waiting) {
-      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-    }
-  },
-});
 
 renderMethod(
   <BrowserRouter>
@@ -39,3 +24,26 @@ renderMethod(
   </BrowserRouter>,
   document.getElementById('root'),
 );
+
+// Old
+// serviceWorker.register({
+//   onUpdate: (registration) => {
+//     isUpdate = true;
+//     setTimeout(() => {
+//       window.location.reload();
+//     }, 5000);
+//     if (registration && registration.waiting) {
+//       registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+//     }
+//   },
+// });
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
